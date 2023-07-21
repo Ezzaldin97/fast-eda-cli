a79cbd9ef453143c6548bd3822f418d1b54315fc
Ezzaldin97 <kingtal632@yahoo.com>
2023-07-21 15:41:27 +0300

    hint of pairwise association

--- src/eda.py
+++ src/eda.py
@@ -2,8 +2,9 @@ import os
 import pandas as pd
 import sweetviz as sv
 from InquirerPy import inquirer
-from InquirerPy.validator import PathValidator
-from typing import Union, List
+from colors import Colors
+
+colors = Colors()
 
 class FastAnalyzer:
     def __init__(self,
@@ -32,16 +33,19 @@ class FastAnalyzer:
             if self.df.shape[1] <= 10:
                 analyze_report = sv.analyze(self.df)
             else:
+                print(f"{colors.yellow}pairwise association will not be created due to large number of features{colors.reset_colors}")
                 analyze_report = sv.analyze(self.df, pairwise_analysis = "off")
         elif eda_type == "Target Analysis":
             if self.df.shape[1] <= 10:
                 analyze_report = sv.analyze(self.df, target_feat = target)
             else:
+                print(f"{colors.yellow}pairwise association will not be created due to large number of features{colors.reset_colors}")
                 analyze_report = sv.analyze(self.df, target_feat = target, pairwise_analysis = "off")
         elif eda_type == "Compare":
             if self.df.shape[1] <= 10:
                 analyze_report = sv.compare(self.df, test_df, target)
             else:
+                print(f"{colors.yellow}pairwise association will not be created due to large number of features{colors.reset_colors}")
                 analyze_report = sv.compare(self.df, test_df, target, pairwise_analysis = "off")
         analyze_report.show_html(os.path.join(os.path.abspath(self.output_path), f"{filename}.html"),
                                  open_browser=False,
